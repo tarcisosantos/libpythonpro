bd50c651d564065919d0e4e2c6555a0429729480
tarcisosantos <paulotarciso@meta.edu.br>
2020-09-15 01:03:53 -0300

    testes automaticos 1

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,28 +1,16 @@
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Tarciso Santos')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
-
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
-    usuarios = [
-        Usuario(nome='Tarciso Santos'),
-        Usuario(nome='Tarciso Santos')
-    ]
-    for usuario in usuarios:
+def test_listar_usuario(sessao):
+     usuarios = [Usuario(nome='Tarciso Santos'),  Usuario(nome='Tais Santos')]
+     for usuario in usuarios:
         sessao.salvar(usuario)
-    assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+     assert usuarios == sessao.listar()
+
+
+
